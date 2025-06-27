c2f9a28aa03339e5f22753902b2e02135653237f
dinphy <dinphy1205@126.com>
2025-06-27 17:49:54 +0800

    chore：优化自定义时间

--- front/components/MemoEdit.vue
+++ front/components/MemoEdit.vue
@@ -264,7 +264,6 @@ onMounted(async () => {
     doubanType.value = ext.doubanBook && ext.doubanBook.title ? 'book' : 'movie'
     doubanData.value = doubanType.value === 'book' ? ext.doubanBook : ext.doubanMovie
     selectedLabel.value = res.tags ? res.tags.substring(0,res.tags.length-1).split(',') : []
-    // 优先显示自定义时间，没有则显示原创建时间
     state.customTime = res.customTime ? dayjs(res.customTime).format("YYYY-MM-DD HH:mm") : 
                                       (res.createdAt ? dayjs(res.createdAt).format("YYYY-MM-DD HH:mm") : '')
   }
